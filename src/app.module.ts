9c4489cd99fbda2db714fe4d97d65f0cb89c9b13
bj.develop.intern7 <bj.develop.intern7@yff.com>
2019-09-25 17:06:25 +0800

    add prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 8080,
+    //   username: 'root',
+    //   password: 'demodb',
+    //   database: 'test',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
