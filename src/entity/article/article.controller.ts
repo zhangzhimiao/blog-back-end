140ff51ef0577bb614f4d6741be2c73e11b170a7
bj.develop.intern7 <bj.develop.intern7@yff.com>
2019-10-09 11:43:46 +0800

    rebuild publish code

--- src/entity/article/article.controller.ts
+++ src/entity/article/article.controller.ts
@@ -1,11 +1,5 @@
 import { Controller, Body, Post, Get, Param } from '@nestjs/common';
 import { ArticleService } from './article.service';
-import { Article } from './article.entity';
-import { service } from 'src/api/service';
-import { Person } from '../person/person.entity';
-import { ArticleCollection } from '../article-collection/article-collection.entity';
-import { Label } from '../label/label.entity';
-import { Column } from '../column/column.entity';
 
 @Controller(`entity/article`)
 export class ArticleController {
@@ -21,44 +15,11 @@ export class ArticleController {
     columnId: string;
     label: string[];
   }) {
-    return service.getPerson(data.personId).then(personData => {
-      return service.getColumn(data.columnId).then(columnData => {
-        const column = new Column();
-        column.id = columnData.data.data.id;
-        column.name = columnData.data.data.name;
-        const date = new Date().getTime() / 1000;
-        const article = new Article();
-        article.content = data.content;
-        article.title = data.title;
-        article.isPublic = parseInt(data.isPublic, 10);
-        article.publishTime = date;
-        const person = new Person();
-        person.id = personData.data.data.id;
-        person.name = personData.data.data.name;
-        person.password = personData.data.data.password;
-        article.person = person;
-        article.columns = [column];
-        const labels = data.label.map(name => {
-          const label = new Label();
-          label.article = article;
-          label.name = name;
-          return label;
-        });
-
-        return this.articleService.publish(article, labels);
-      });
-    });
+    return this.articleService.publish(data);
   }
 
   @Get('detail/:id')
   getArticleDetail(@Param('id') id: string) {
     return this.articleService.getArticleDetail(id);
-  }
-
-  @Post('collect')
-  collect(@Body('data') data: { personId: string; articleId: string }) {
-    const date = new Date().getTime() / 1000;
-    const articleCollection = new ArticleCollection();
-    articleCollection.collectTime = date;
   }
 }
--- src/entity/article/article.service.ts
+++ src/entity/article/article.service.ts
@@ -4,6 +4,9 @@ import { Article } from './article.entity';
 import { Repository, getManager, getConnection } from 'typeorm';
 import { BackData } from 'src/types/response';
 import { Label } from '../label/label.entity';
+import { service } from 'src/api/service';
+import { Column } from '../column/column.entity';
+import { Person } from '../person/person.entity';
 
 @Injectable()
 export class ArticleService {
@@ -12,18 +15,50 @@ export class ArticleService {
     private readonly articleRepository: Repository<Article>,
   ) {}
 
-  async publish(article: Article, labels: Label[]): Promise<BackData> {
-    getConnection().manager.save(article);
-    getConnection().manager.save(labels);
-
-    return Promise.resolve()
-      .then(() => ({ code: 0, data: {} }))
-      .catch(e => ({
-        code: 1,
-        data: {
-          message: 'publish error',
-        },
-      }));
+  async publish(data: {
+    personId: string;
+    content: string;
+    isPublic: string;
+    title: string;
+    columnId: string;
+    label: string[];
+  }): Promise<BackData> {
+    return service.getPerson(data.personId).then(personData => {
+      return service.getColumn(data.columnId).then(columnData => {
+        const column = new Column();
+        column.id = columnData.data.data.id;
+        column.name = columnData.data.data.name;
+        const date = new Date().getTime() / 1000;
+        const article = new Article();
+        article.content = data.content;
+        article.title = data.title;
+        article.isPublic = parseInt(data.isPublic, 10);
+        article.publishTime = date;
+        const person = new Person();
+        person.id = personData.data.data.id;
+        person.name = personData.data.data.name;
+        person.password = personData.data.data.password;
+        article.person = person;
+        article.columns = [column];
+        const labels = data.label.map(name => {
+          const label = new Label();
+          label.article = article;
+          label.name = name;
+          return label;
+        });
+        return Promise.all([
+          getConnection().manager.save(article),
+          getConnection().manager.save(labels),
+        ])
+          .then(([d1, d2]) => ({ code: 0, data: { d2 } }))
+          .catch(e => ({
+            code: 1,
+            data: {
+              message: 'publish error',
+            },
+          }));
+      });
+    });
   }
 
   async getArticleDetail(id: string) {
